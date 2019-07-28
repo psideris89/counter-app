2a2ba2ffc66ff8c44a8e745accaf08c66e6532e6
ps <psideris1989@gmail.com>
2019-07-28 12:25:38 +0100

    Add list which is rendered dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,8 @@ import { throwStatement, thisExpression } from '@babel/types';
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   render() {
@@ -11,6 +12,11 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
